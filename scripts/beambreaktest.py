96de205e5a31d59ba0e52af6d7392941f3538409
rivques <38469076+rivques@users.noreply.github.com>
2025-01-14 13:35:31 -0500

    test beam breaks and start planning wiring

---
+++ scripts/beambreaktest.py
@@ -0,0 +1,15 @@
+import board
+import digitalio
+
+break_beam = digitalio.DigitalInOut(board.GP5)
+break_beam.direction = digitalio.Direction.INPUT
+break_beam.pull = digitalio.Pull.UP
+
+led_built_in = digitalio.DigitalInOut(board.LED)
+led_built_in.direction = digitalio.Direction.OUTPUT
+
+while True:
+    if break_beam.value:
+        led_built_in.value = False
+    else:
+        led_built_in.value = True
